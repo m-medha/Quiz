5e0dd6d17f8cd8d3b7a0c9bc3f867a0f72cf7958
M Medha <157206385+m-medha@users.noreply.github.com>
2024-06-26 21:20:03 +0530

    Add files via upload

---
+++ quiz.java
@@ -0,0 +1,222 @@
+import java.util.*;
+class quiz
+{
+    public static void main()
+    {
+        Scanner ob = new Scanner(System.in);
+        System.out.println("$#$# WELCOME TO THE QUIZ #$#$");
+        System.out.println("******************************");
+        System.out.println("@$@$ THERE ARE 10 QUESTIONS, EACH QUESTION HAS 4 OPTIONS,CHOOSE THE CORRECT ANSWER $@$@");
+    {
+        System.out.println("                           ");
+        System.out.println("QUESTION 1");
+        System.out.println("WHICH IS THE LARGEST COFFEE PRODUCING STATE IN INDIA?");
+        System.out.println("1 - KERALA" );
+        System.out.println("2 - TAMIL NADU");
+        System.out.println("3 - KARNATAKA");
+        System.out.println("4 - ARUNACHAL PRADESH");
+        System.out.println("ENTER YOUR CHOICE");
+        int ch1 = ob.nextInt();
+        {
+            if(ch1==3)
+            {
+                System.out.println("YOUR CHOICE IS CORRECT!!!");
+            }
+            else 
+            {
+                System.out.println("YOUR CHOICE IS WRONG!!!");
+                System.out.println("THE CORRECT ANSWER IS 3 - KARNATAKA");
+            }
+        }
+    }
+    {
+        System.out.println("                           ");
+        System.out.println("QUESTION 2");
+        System.out.println("THE WORLD'S LARGEST DESERT IS?");
+        System.out.println("1 - THAR");
+        System.out.println("2 - KALAHARI");
+        System.out.println("3 - SONARAN");
+        System.out.println("4 - SAHARA");
+        int ch2 = ob.nextInt();
+        {
+            if(ch2==4)
+            {
+                System.out.println("YOUR CHOICE IS CORRECT!!!");
+            }
+            else
+            {
+                System.out.println("YOUR CHOICE IS WRONG!!!");
+                System.out.println("THE CORRECT ANSWER IS 4 - SAHARA"); 
+            }
+        }
+    }
+    {
+        System.out.println("                             ");
+        System.out.println("QUESTION 3");
+        System.out.println("THE EASTERN GHATS AND WESTERN GHATS MEET AT?");
+        System.out.println("1 - ANNAMALAI HILLS");
+        System.out.println("2 - NILIGIRI HILLS");
+        System.out.println("3 - PALANI HILLS");
+        System.out.println("4 - CARDAMOM HILLS");
+        int ch3 = ob.nextInt();
+        {
+            if(ch3==2)
+            {
+                System.out.println("YOUR CHOICE IS CORRECT!!!");
+            }
+            else
+            {
+                System.out.println("YOUR CHOICE IS WRONG!!!");
+                System.out.println("THE CORRECT ANSWER IS 2 - NILIGIRI HILLS");
+            }
+        }
+    }
+    {
+        System.out.println("                                ");
+        System.out.println("QUESTION 4");
+        System.out.println("WHICH STATE IN INDIA HAS THE SMALLEST LAND AREA?");
+        System.out.println("1 - GOA");
+        System.out.println("2- NAGALAND");
+        System.out.println("3 - SIKKIM");
+        System.out.println("4 - TRIPURA");
+        int ch4 = ob.nextInt();
+        {
+            if(ch4==1)
+            {
+                System.out.println("YOUR CHOICE IS CORRECT!!!");
+            }
+            else
+            {
+                System.out.println("YOUR CHOICCE IS WRONG!!!");
+                System.out.println("THE CORRECT ANSWER IS 1 - GOA");
+            }
+        }
+    }
+    {
+        System.out.println("                                ");
+        System.out.println("QUESTION 5");
+        System.out.println(" WHICH IS THE LARGEST UNION TERRITORIES OF INDIA?");
+        System.out.println("1 - PUDUCHERRY");
+        System.out.println("2 - LAKSHADWEEP");
+        System.out.println("3 - DAMAN AND DIU");
+        System.out.println("4 - CHANDIGARH");
+        int ch5 = ob.nextInt();
+        {
+            if(ch5==1)
+            {
+                System.out.println("YOUR CHOICE IS CORRECT!!!");
+            }
+            else
+            {
+                System.out.println("YOUR CHOICE IS WRONG!!!");
+                System.out.println("THE CORRECT ANSWER IS 1 - PUDUCHERRY");
+            }
+        }
+    }
+    {
+        System.out.println("                                ");
+        System.out.println("QUESTION 6");
+        System.out.println("THE MOST ECONOMICALLY IMPORTANT FORESTS OF INDIA?");
+        System.out.println("1 - TIDAL FOREST");
+        System.out.println("2 - THORN FOREST");
+        System.out.println("3 - EVERGREEN FOREST");
+        System.out.println("4 - TROPICAL DECIDUOUS FOREST");
+        int ch6 = ob.nextInt();
+        {
+            if(ch6==4)
+            {
+                System.out.println("YOUR CHOICE IS CORRECT!!!");
+            }
+            else
+            {
+                System.out.println("YOUR CHOICE IS WRONG!!!");
+                System.out.println("THE CORRECT ANSWER IS 4 - TROPICAL DECIDUOUS FOREST");
+            }
+        }
+    }
+    {
+        System.out.println("                                ");
+        System.out.println("QUESTION 7");
+        System.out.println("THE PLATUEA RICH IN MINERALS IN INDIA IS?");
+        System.out.println("1 - MALWA PLATEAU");
+        System.out.println("2 - CHHOTA NAGPUR PLATEAU");
+        System.out.println("3 - DECCAN PLATEAU");
+        System.out.println("4 - MYSURU PATEAU");
+        int ch7 = ob.nextInt();
+        {
+             if(ch7==2)
+             {
+                 System.out.println("YOUR CHOICE IS CORRECT!!!");
+             }
+             else
+             {
+                 System.out.println("YOUR CHOICE IS WRONG!!!");
+                 System.out.println("THE CORRECT ANSWER IS 2 - CHHOTA NAGPUR PLATEAU");
+             }
+        }
+    }
+    {
+        System.out.println("                                  ");
+        System.out.println("QUESTION 8");
+        System.out.println("THE NATURAL GAPS ACROSS THE MOUNTAINS WHICH PROVIDE ROUTES ARE CALLED?");
+        System.out.println("1 - PEAKS");
+        System.out.println("2 - DUNES");
+        System.out.println("3 - PASSES");
+        System.out.println("4 - PLATEAUS");
+        int ch8 = ob.nextInt();
+        {
+            if(ch8==3)
+            {
+                System.out.println("YOUR CHOICE IS CORRECT!!!");
+            }
+            else
+            {
+                System.out.println("YOUR CHOICE IS WRONG!!!");
+                System.out.println("THE CORRECT ANSWER IS 3 - PASSES");
+            }
+        }
+    }
+    {
+        System.out.println("                                  ");
+        System.out.println("QUESTION 9");
+        System.out.println("THE NATIONAL PARK, 'VALLEY OF FLOWERS' LIES IN THE STATE OF?");
+        System.out.println("1- UTTARKHAND");
+        System.out.println("2 - KERALA");
+        System.out.println("3 - JAMMU AND KASHMIR");
+        System.out.println("4 - HIMACHAL PRADESH");
+        int ch9 = ob.nextInt();
+        {
+            if(ch9==1)
+            {
+                System.out.println("YOUR CHOICE IS CORRECT!!!");
+            }
+            else
+            {
+                System.out.println("YOUR CHOICE IS WRONG!!!");
+                System.out.println("THE CORRECT ANSWER IS 1 - UTTARKHAND");
+            }
+        }
+    }
+    {
+        System.out.println("                                  ");
+        System.out.println("QUESTION 10");
+        System.out.println("THE LEADING STATE IN PRODUCING PAPER IS?");
+        System.out.println("1 - KERALA");
+        System.out.println("2 - ODISHA");
+        System.out.println("3 - WEST BENGAL");
+        System.out.println("4 - BIHAR");
+        int ch10 = ob.nextInt();
+        {
+            if(ch10==3)
+            {
+                System.out.println("YOUR CHOICE IS CORRECT!!!");
+            }
+            else
+            {
+                System.out.println("YOUR CHOICE IS WRONG!!!");
+                System.out.println("THE CORRECT ANSWER IS 3 - WEST BENGAL");
+            }
+        }
+    }
+}
+}
